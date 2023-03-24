8ffa94b8bb56ef410209eef4d542fe5c066e8fc9
Zanetaa <91408269+Zanetaa@users.noreply.github.com>
2023-03-24 20:38:18 +0100

    c

--- src/index.jsx
+++ src/index.jsx
@@ -3,26 +3,22 @@ import { createRoot } from 'react-dom/client';
 import './style.css';
 
 const App = () => {
-  return (
-    <div className="container">
-      <header>
-        <div className="logo" />
-        <h1>React webová aplikace</h1>
-      </header>
-      <main>
-        <p>
-          Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí
-          <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>
-          .
-        </p>
-      </main>
-      <footer>
-        <p>Czechitas, Digitální akademie: Web</p>
-      </footer>
-    </div>
-  );
+  const redSlider = document.getElementById('redSlider');
+  const greenSlider = document.getElementById('greenSlider');
+  const blueSlider = document.getElementById('blueSlider');
+  const colorBox = document.getElementById('color-box');
+
+  const changeColor = () => {
+    const redValue = redSlider.value;
+    const greenValue = greenSlider.value;
+    const blueValue = blueSlider.value;
+    const color = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
+    colorBox.style.backgroundColor = color;
+  };
+
+  redSlider.addEventListener('input', changeColor);
+  greenSlider.addEventListener('input', changeColor);
+  blueSlider.addEventListener('input', changeColor);
 };
 
-createRoot(
-  document.querySelector('#app'),
-).render(<App />);
+createRoot(document.querySelector('#app')).render(<App />);
