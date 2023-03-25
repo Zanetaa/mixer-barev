e0f0fd351de02a5a7b63915fcb06959b331177d1
Zanetaa <91408269+Zanetaa@users.noreply.github.com>
2023-03-25 10:40:00 +0100

    zoobrazuje se mi to bílé a nemixuje se

--- src/ColorSlider/index.jsx
+++ src/ColorSlider/index.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './style.css';
 
-export const ColorSlider = ({ baseColor, colorName, onValueChange }) => {
+export const ColorSlider = ({ baseColor, colorName, onValueChange, value }) => {
   return (
     <>
       <label htmlFor={baseColor}>{colorName}</label>
       <input
         type="range"
-        className="slider slider--red"
-        id="redSlider"
+        className={'slider slider--${baseColor}'}
+        id={'${baseColor}Slider'}
         min="0"
         max="255"
-        value="0"
+        value={value}
         onChange={(event) => {
           onValueChange(event.target.value);
         }}
